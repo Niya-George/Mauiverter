1aafcd5e142fa191e55b237aa1af727f4dfa4fcd
Niya-George <niya.nediyathara@people10.com>
2024-07-22 11:37:10 +0530

    Initial commit

---
+++ Mauiverter/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Mauiverter.MVVM.ViewModel;
+using Mauiverter.MVVM.Views;
+
+namespace Mauiverter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MenuView();
+        }
+    }
+}
